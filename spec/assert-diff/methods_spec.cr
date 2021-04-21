15b212cdf9cc3ad2e254532fb5ee8aedd727dff9
Yusuke Hosonuma <tobi462@gmail.com>
2021-04-21 17:11:59 +0900

    add README spec

--- spec/assert-diff/methods_spec.cr
+++ spec/assert-diff/methods_spec.cr
@@ -1,7 +1,7 @@
 require "../spec_helper"
 
 struct Rectangle
-  def initialize(@origin : Point, @width : Int32, @height : Int32)
+  def initialize(@origin : Point, @width : Int32, @height : Int32, @comment : String)
   end
 end
 
@@ -10,10 +10,19 @@ struct Point
   end
 end
 
+describe "README.md" do
+  pending "example" do
+    expected = Rectangle.new(Point.new(0, 1), 4, 7, "One\nTwo\nThree\nFour")
+    actual = Rectangle.new(Point.new(0, 0), 4, 3, "Zero\nOne\nTwo!!\nThree")
+
+    actual.should eq_diff expected # or use `eq_full_diff`
+  end
+end
+
 describe ".print_diff" do
   it "has no diff" do
     io = IO::Memory.new
-    a = Rectangle.new(Point.new(0, 0), 4, 3)
+    a = Rectangle.new(Point.new(0, 0), 4, 3, "One\nTwo\nThree\nFour")
     b = a
     print_diff(a, b, io)
     io.to_s.gsub(/\e.+?m/, "").should eq <<-EOF
@@ -23,11 +32,20 @@ describe ".print_diff" do
 
   it "has diff" do
     io = IO::Memory.new
-    a = Rectangle.new(Point.new(0, 0), 4, 3)
-    b = Rectangle.new(Point.new(0, 1), 4, 7)
+    a = Rectangle.new(Point.new(0, 0), 4, 3, "One\nTwo\nThree\nFour")
+    b = Rectangle.new(Point.new(0, 1), 4, 7, "Zero\nOne\nTwo!!\nThree")
     print_diff(a, b, io)
-    io.to_s.gsub(/\e.+?m/, "").should eq <<-EOF
+    io.to_s.gsub(/\e.+?m/, "").should eq_diff <<-EOF
       Rectangle {
+        comment:
+          ```
+    +     Zero
+          One
+    +     Two!!
+    -     Two
+          Three
+    -     Four
+          ```,
     -   height: 3,
     +   height: 7,
         origin: Point {
@@ -44,7 +62,7 @@ end
 describe ".print_diff_full" do
   it "has no diff" do
     io = IO::Memory.new
-    a = Rectangle.new(Point.new(0, 0), 4, 3)
+    a = Rectangle.new(Point.new(0, 0), 4, 3, "One\nTwo\nThree\nFour")
     b = a
     print_diff_full(a, b, io)
     io.to_s.gsub(/\e.+?m/, "").should eq <<-EOF
@@ -54,11 +72,20 @@ describe ".print_diff_full" do
 
   it "has diff" do
     io = IO::Memory.new
-    a = Rectangle.new(Point.new(0, 0), 4, 3)
-    b = Rectangle.new(Point.new(0, 1), 4, 7)
+    a = Rectangle.new(Point.new(0, 0), 4, 3, "One\nTwo\nThree\nFour")
+    b = Rectangle.new(Point.new(0, 1), 4, 7, "Zero\nOne\nTwo!!\nThree")
     print_diff_full(a, b, io)
     io.to_s.gsub(/\e.+?m/, "").should eq <<-EOF
       Rectangle {
+        comment:
+          ```
+    +     Zero
+          One
+    +     Two!!
+    -     Two
+          Three
+    -     Four
+          ```,
     -   height: 3,
     +   height: 7,
         origin: Point {
